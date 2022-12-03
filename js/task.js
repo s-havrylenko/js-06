8b859641f85e2994b14be27198ed5a089d946372
Alona1m <sergei.ua.kh@gmail.com>
2022-12-03 18:00:01 +0200

    Adds task-02

--- js/task.js
+++ js/task.js
@@ -1,9 +1,18 @@
 'use strict';
 import users from "./users.js";
 
+
 //1. Получить массив имен всех пользователей (поле name).
 
 const getUserNames = users => users.map(user => user.name);
 
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+
+// 2. Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
+
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+console.log(getUsersWithEyeColor(users, 'green'));
