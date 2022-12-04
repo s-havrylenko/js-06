65e2707ad2629873b1058cd2da1e01a68adecb9d
Alona1m <sergei.ua.kh@gmail.com>
2022-12-04 17:08:20 +0200

    Adds task-05

--- js/task.js
+++ js/task.js
@@ -42,3 +42,11 @@ console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazq
 const getInactiveUsers = users => users.filter(user => !user.isActive);
 
 console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+
+//5. Получить пользоваля (не массив) по email (поле email, он уникальный).
+
+const getUserWithEmail = (users, email) => users.find(user => user.email === email);
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
