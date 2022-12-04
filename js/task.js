75b299931f9eb096c4d753edf7ff87772380b151
Alona1m <sergei.ua.kh@gmail.com>
2022-12-04 17:24:14 +0200

    Adds task-06

--- js/task.js
+++ js/task.js
@@ -50,3 +50,13 @@ const getUserWithEmail = (users, email) => users.find(user => user.email === ema
 
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
 console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+
+
+//6. Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+
+const getUsersWithAge = (users, min, max) => users.filter(user => user.age >= min && user.age <= max);
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
